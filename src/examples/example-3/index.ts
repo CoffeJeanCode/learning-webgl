7d8e8903388ae0560f499b760578eb02002edb98
Jean Pierre Ortiz Murcia <45444014+CoffeJeanCode@users.noreply.github.com>
2023-08-21 03:57:11 +0000

    create: example 3

---
+++ src/examples/example-3/index.ts
@@ -0,0 +1,62 @@
+import Addons, {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+  Color,
+} from "../../lib/three";
+const Addon = Addons.default;
+
+let scene: Scene,
+  camera: PerspectiveCamera,
+  renderer: WebGLRenderer,
+  cube: Mesh,
+  material: MeshBasicMaterial,
+  controls: any;
+
+let hue = 0;
+let alpha = 0;
+export const setup = withInfo(
+  "Example 3",
+  "Change color material and scale"
+)(() => {
+  scene = new Scene();
+  camera = new PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  renderer = new WebGLRenderer();
+  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+  appendApp(renderer.domElement);
+
+  controls = new Addon.OrbitControls(camera, renderer.domElement);
+  const geometry = new BoxGeometry(1, 1, 1);
+  material = new MeshBasicMaterial({ color: 0x191d88 });
+  cube = new Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.set(0, 0, 2);
+});
+
+export const loop = () => {
+  hue = (hue + 0.005) % 1;
+  const color = new Color().setHSL(hue, 1, 0.5);
+
+  material.color.copy(color);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  cube.rotation.z += 0.01;
+
+  const scale = Math.sin(alpha);
+  cube.scale.set(scale, Math.tan(scale), scale);
+
+  alpha += 0.01;
+  controls.update();
+
+  renderer.render(scene, camera);
+};
